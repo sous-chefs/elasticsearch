81107ef2e447c0b3a9d0eacf21ecf0baf6431932
Karel Minarik <karmi@karmi.cz>
2011-12-27 12:09:59 +0100

    Allow to declare users for Nginx proxy in node configuration
    * Moved the retrieving of Nginx HTTP allowed users to attributes configuration
    * Use regular `node.elasticsearch[:nginx][:users]` in the "proxy_nginx" recipe

--- attributes/proxy_nginx.rb
+++ attributes/proxy_nginx.rb
@@ -1,7 +1,15 @@
-# === NGINX ===
+# Try to load data bag item 'elasticsearch/aws' ------------------
+#
+users = Chef::DataBagItem.load('elasticsearch', 'users')['users'] rescue []
+# ----------------------------------------------------------------
 
+# === NGINX ===
+# Allowed users are set based on data bag values.
+# You may choose to configure them in your node configuration instead.
+#
 default.elasticsearch[:nginx][:port]           = "8080"
 default.elasticsearch[:nginx][:dir]            = "/etc/nginx"
 default.elasticsearch[:nginx][:user]           = "nginx"
 default.elasticsearch[:nginx][:log_dir]        = "/var/log/nginx"
+default.elasticsearch[:nginx][:users]          = users
 default.elasticsearch[:nginx][:passwords_file] = "#{node.elasticsearch[:conf_path]}/passwords"
--- recipes/proxy_nginx.rb
+++ recipes/proxy_nginx.rb
@@ -6,11 +6,7 @@ template "#{node.elasticsearch[:nginx][:dir]}/conf.d/elasticsearch_proxy_nginx.c
   notifies :restart, resources(:service => "nginx")
 end
 
-# Try to load data bag item <https://manage.opscode.com/databags/elasticsearch/items/users>
-#
-users = data_bag_item('elasticsearch', 'users')['users'] rescue []
-
-unless users.empty?
+unless node.elasticsearch[:nginx][:users].empty?
 
   ruby_block "add users to passwords file" do
 
@@ -19,7 +15,7 @@ unless users.empty?
       require 'webrick/httpauth/htpasswd'
       @htpasswd = WEBrick::HTTPAuth::Htpasswd.new(node.elasticsearch[:nginx][:passwords_file])
 
-      users.each do |u|
+      node.elasticsearch[:nginx][:users].each do |u|
         STDOUT.print "Adding user '#{u['username']}' to #{node.elasticsearch[:nginx][:passwords_file]}\n"
         @htpasswd.set_passwd( 'Elasticsearch', u['username'], u['password'] )
       end
@@ -31,6 +27,8 @@ unless users.empty?
 
 end
 
+# Ensure proper permissions and existence of the passwords file
+#
 file node.elasticsearch[:nginx][:passwords_file] do
   owner node.elasticsearch[:nginx][:user] and group node.elasticsearch[:nginx][:user] and mode 0755
   action :touch
