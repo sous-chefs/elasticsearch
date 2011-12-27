d940ab064c9503f54860d2cc738cfa28635e4d77
Karel Minarik <karmi@karmi.cz>
2011-12-27 13:05:48 +0100

    [FIX] Re-use the Nginx configuration from the "nginx" cookbook by default

--- attributes/proxy_nginx.rb
+++ attributes/proxy_nginx.rb
@@ -8,8 +8,8 @@ users = Chef::DataBagItem.load('elasticsearch', 'users')['users'] rescue []
 # You may choose to configure them in your node configuration instead.
 #
 default.elasticsearch[:nginx][:port]           = "8080"
-default.elasticsearch[:nginx][:dir]            = "/etc/nginx"
-default.elasticsearch[:nginx][:user]           = "nginx"
-default.elasticsearch[:nginx][:log_dir]        = "/var/log/nginx"
+default.elasticsearch[:nginx][:dir]            = ( node.nginx[:dir]     rescue '/etc/nginx'     )
+default.elasticsearch[:nginx][:user]           = ( node.nginx[:user]    rescue 'nginx'          )
+default.elasticsearch[:nginx][:log_dir]        = ( node.nginx[:log_dir] rescue '/var/log/nginx' )
 default.elasticsearch[:nginx][:users]          = users
 default.elasticsearch[:nginx][:passwords_file] = "#{node.elasticsearch[:conf_path]}/passwords"
