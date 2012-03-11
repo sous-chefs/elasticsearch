5b8a0ef8c056c0893e30e9fa0beca17214e21a8f
Karel Minarik <karmi@karmi.cz>
2012-03-11 11:26:58 +0100

    [FIX] Fix a failed provisioning where `$ES_HOME/plugins` does not exist on the first run

--- definitions/install_plugin.rb
+++ definitions/install_plugin.rb
@@ -11,7 +11,7 @@ define :install_plugin do
     not_if do
       Dir.entries("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/").any? do |plugin|
         params[:name].include? plugin
-      end
+      end rescue false
     end
 
   end
