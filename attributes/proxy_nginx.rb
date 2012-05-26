8285d11e4fcb8b659159357e7ed7be351bc95c20
Karel Minarik <karmi@karmi.cz>
2012-05-26 22:29:43 +0200

    [CONFIG] Changed, that authenticated access to the Cluster API via Nginx proxy is configurable in attributes

--- attributes/proxy_nginx.rb
+++ attributes/proxy_nginx.rb
@@ -4,8 +4,9 @@ users = Chef::DataBagItem.load('elasticsearch', 'users')['users'] rescue []
 # ----------------------------------------------------------------
 
 # === NGINX ===
-# Allowed users are set based on data bag values.
-# You may choose to configure them in your node configuration instead.
+# Allowed users are set based on data bag values, when it exists.
+#
+# It's possible to define the credentials directly in your node configuration, if your wish.
 #
 default.elasticsearch[:nginx][:port]           = "8080"
 default.elasticsearch[:nginx][:dir]            = ( node.nginx[:dir]     rescue '/etc/nginx'     )
@@ -13,3 +14,9 @@ default.elasticsearch[:nginx][:user]           = ( node.nginx[:user]    rescue '
 default.elasticsearch[:nginx][:log_dir]        = ( node.nginx[:log_dir] rescue '/var/log/nginx' )
 default.elasticsearch[:nginx][:users]          = users
 default.elasticsearch[:nginx][:passwords_file] = "#{node.elasticsearch[:conf_path]}/passwords"
+
+# Deny or allow authenticated access to cluster API.
+#
+# Set this to `true` if you want to use a tool like BigDesk
+#
+default.elasticsearch[:nginx][:allow_cluster_api] = false
