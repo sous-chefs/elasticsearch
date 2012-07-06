43604c22db88e0de0ae86137e94527f568111d23
Karel Minarik <karmi@karmi.cz>
2012-07-07 00:01:21 +0200

    [#17] Cleaned up the Minitest specs
    * Use `wait_for_status` parameter of Cluster Health API for waiting until elasticsearch is up
    * DRY up the URLs
    * Silence `curl` transfer stats output
    * Add `tests` symlink

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -8,6 +8,9 @@ describe_recipe 'elasticsearch::default' do
   require 'net/http'
   require 'json'
 
+  cluster_url = 'http://localhost:9200'
+  health_url  = "#{cluster_url}/_cluster/health"
+
   it "runs as a daemon" do
     service("elasticsearch").must_be_running
   end
@@ -17,41 +20,37 @@ describe_recipe 'elasticsearch::default' do
   end
 
   it "shows green or yellow for cluster health" do
-    # let's wait until the service is alive
-    health_url = 'http://localhost:9200'
+    # Let's wait until the service is alive
     timeout = 120
-    until system("curl #{health_url}") or timeout == 0
-       sleep 1
-       timeout -= 1
+    until system("curl --silent --show-error '#{health_url}?wait_for_status=yellow&timeout=1m'") or timeout == 0
+      sleep 1
+      timeout -= 1
     end
 
-    cluster_url = URI.parse health_url
-    resp = Net::HTTP.get_response cluster_url
+    resp = Net::HTTP.get_response URI.parse(health_url)
     status = JSON.parse(resp.read_body)['status']
     assert status != "red"
   end
 
   it "test data is written and read back" do
-    # let's wait until the service is alive
-    test_url = 'http://localhost:9200/'
-
+    # Let's wait until the service is alive
     timeout = 120
-    until system("curl http://localhost:9200") or timeout == 0
-       sleep 1
-       timeout -= 1
+    until system("curl --silent --show-error '#{health_url}?wait_for_status=yellow&timeout=1m'") or timeout == 0
+      sleep 1
+      timeout -= 1
     end
 
-    # let's clean up first
-    system("curl -X DELETE #{test_url}/test_chef_cookbook")
+    # Let's clean up first
+    system("curl --silent --show-error -X DELETE #{cluster_url}/test_chef_cookbook")
 
-    # insert test data
+    # Insert test data
     (1..5).each do |num|
-      test_uri = URI.parse "#{test_url}/test_chef_cookbook/document/#{num}"
-      system(%|curl http://localhost:9200/test_chef_cookbook/document/#{num} -d '{ "title": "Test #{num}", "time": "#{Time.now.utc}", "enabled": true }'|)
+      test_uri = URI.parse "#{cluster_url}/test_chef_cookbook/document/#{num}"
+      system(%Q|curl --silent --show-error #{cluster_url}/test_chef_cookbook/document/#{num} -d '{ "title": "Test #{num}", "time": "#{Time.now.utc}", "enabled": true }'|)
     end
 
-    Net::HTTP.post_form URI.parse("#{test_url}/test_chef_cookbook/_refresh"), {}
-    resp = Net::HTTP.get_response(URI.parse("#{test_url}/test_chef_cookbook/_search?q=Test&size=1"))
+    Net::HTTP.post_form URI.parse("#{cluster_url}/test_chef_cookbook/_refresh"), {}
+    resp = Net::HTTP.get_response URI.parse("#{cluster_url}/test_chef_cookbook/_search?q=Test")
     total_hits = JSON.parse(resp.read_body)['hits']['total']
 
     assert total_hits == 5
