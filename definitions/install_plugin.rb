a00515e2272e71257f522c17eeeb78eac61e746f
Karel Minarik <karmi@karmi.cz>
2012-07-07 13:13:44 +0200

    [FIX] Fixed errors in the "install_plugin" definition
    * Incorrect user for the script, directory belonged to "root"
    * Skip "." and ".." in the `not_if` loop

--- definitions/install_plugin.rb
+++ definitions/install_plugin.rb
@@ -1,15 +1,19 @@
 # Install ElasticSearch plugin
 #
+# TODO: Make it a Chef LWRP [http://wiki.opscode.com/display/chef/Lightweight+Resources+and+Providers+(LWRP)]
+# TODO: Install plugins based on node attribute, data bag values, etc
+#
 define :install_plugin do
 
   bash "/usr/local/bin/plugin -install #{params[:name]}" do
-    user "root"
+    user node.elasticsearch[:user]
     code "/usr/local/bin/plugin -install #{params[:name]}"
 
     notifies :restart, resources(:service => 'elasticsearch')
 
     not_if do
       Dir.entries("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/").any? do |plugin|
+        next if plugin =~ /^\./
         params[:name].include? plugin
       end rescue false
     end
