884c45a4c878d3762704720501625ba7e1fa73f2
Jack Kordas <Jack.Kordas@gmail.com>
2012-08-28 15:49:35 -0500

    provide default for rspec

--- attributes/default.rb
+++ attributes/default.rb
@@ -31,6 +31,7 @@ default.elasticsearch[:pid_path]  = "/usr/local/var/run/elasticsearch"
 # Maximum amount of memory to use is automatically computed as 2/3 of total available memory.
 # You may choose to configure it in your node configuration instead.
 #
+default[:memory][:total] = "2000000kB"
 max_mem = "#{(node.memory.total.to_i - (node.memory.total.to_i/3) ) / 1024}m"
 default.elasticsearch[:min_mem]  = "128m"
 default.elasticsearch[:max_mem]  = max_mem
