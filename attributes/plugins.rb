248ad6896537331fe5a308891a989b31973f3ef1
Karel Minarik <karmi@karmi.cz>
2012-12-12 19:58:30 +0100

    Cleaned up log messages about plugins
    Closes #12

--- attributes/plugins.rb
+++ attributes/plugins.rb
@@ -1,8 +1,8 @@
-Chef::Log.debug "Attempting to loading plugin list from the databag..."
+Chef::Log.debug "Attempting to load plugin list from the databag..."
 
 plugins = Chef::DataBagItem.load('elasticsearch', 'plugins').to_hash['plugins'] rescue {}
 
-Chef::Log.debug "Loaded these plugins: #{plugins.keys}"
+Chef::Log.debug "Plugins list: #{plugins.keys.inspect}"
 
 default.elasticsearch[:plugins] = plugins
 default.elasticsearch[:plugins_mandatory] ||= []
