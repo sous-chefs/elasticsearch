b7350ac0df34f0e991fe1c19eebc98a7bae9efb4
Karel Minarik <karmi@karmi.cz>
2012-12-18 19:47:29 +0100

    [FIX] Ensure proper permissions for the plugins directory after installation

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,3 +1,10 @@
 node[:elasticsearch][:plugins].each do | name, config |
   install_plugin name, config
 end
+
+directory "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/" do
+  owner node.elasticsearch[:user]
+  group node.elasticsearch[:user]
+  mode 0755
+  recursive true
+end
