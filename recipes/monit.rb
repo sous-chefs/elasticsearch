02e22d4f18e07b70869f8351205c2037c234461b
Rory Fitzpatrick <rory@fitzpatrick.fm>
2013-01-07 21:46:09 +0000

    Specifies template cookbook for monit recipe.
    Fixes #29.

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -1,8 +1,10 @@
 # Add Monit configuration file via the `monitrc` definition
 #
 begin
-  monitrc("elasticsearch",
-          :pidfile => "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node_name].to_s.gsub(/\W/, '_')}.pid")
+  monitrc "elasticsearch" do
+    pidfile "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node_name].to_s.gsub(/\W/, '_')}.pid"
+    template_cookbook "elasticsearch"
+  end
 rescue Exception => e
   Chef::Log.error "The 'monit' recipe is not included in the node run_list or the 'monitrc' resource is not defined"
   raise e
