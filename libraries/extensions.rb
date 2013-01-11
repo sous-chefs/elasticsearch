f681986919b7ec4f448bdd882842b9ed980b1579
Srivatsa Katta <vatsa.katta@gmail.com>
2013-01-11 18:32:18 +0530

    Katta: Moving library files from subfolder extensions to the root library folder.
    Because of an open bug in Chef http://tickets.opscode.com/browse/CHEF-672

--- libraries/extensions.rb
+++ libraries/extensions.rb
@@ -1,4 +1,4 @@
-Dir.glob( [ File.expand_path('../extensions', __FILE__), '*.rb' ].join('/') ).each do |lib|
+Dir.glob( [ File.expand_path('../', __FILE__), '*.rb' ].join('/') ).each do |lib|
   Chef::Log.debug "Loading extension: #{File.basename(lib)}"
   require lib
 end
