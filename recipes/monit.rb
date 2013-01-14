f133f6d553f3e9d5dc33129b3ca1fb3d4eea516e
Karel Minarik <karmi@karmi.cz>
2013-01-14 14:08:13 +0100

    [DOC] Added a note about problems with Monit configuration on certain systems
    Example: Amazon Linux. Should be handled better by the Monit cookbook.

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -10,3 +10,9 @@ rescue Exception => e
   Chef::Log.error "The 'monit' recipe is not included in the node run_list or the 'monitrc' resource is not defined"
   raise e
 end
+
+# NOTE: On some systems, notably Amazon Linux, Monit installed from packages
+#       has a different configuration file then expected by the Monit
+#       cookbook. In such case:
+#
+#       sudo cp /etc/monit/monitrc /etc/monit.conf
