7ffa1492bde66502658cbcc4fb4c7b06f6bc16f6
Karel Minarik <karmi@karmi.cz>
2013-01-16 12:33:31 +0100

    [TEST] Fixed failing tests in "cluster_test.rb"

--- tests/cluster_test.rb
+++ tests/cluster_test.rb
@@ -42,12 +42,13 @@ describe_recipe 'elasticsearch::default' do
       system("curl --silent --show-error -X DELETE #{cluster_url}/test_chef_cookbook")
 
       # Insert test data
+      system(%Q|curl --silent --show-error -X PUT #{cluster_url}/test_chef_cookbook -d '{"index":{"number_of_shards":1,"number_of_replicas":0}}'|)
       (1..5).each do |num|
         test_uri = URI.parse "#{cluster_url}/test_chef_cookbook/document/#{num}"
-        system(%Q|curl --silent --show-error #{cluster_url}/test_chef_cookbook/document/#{num} -d '{ "title": "Test #{num}", "time": "#{Time.now.utc}", "enabled": true }'|)
+        system(%Q|curl --silent --show-error -X PUT #{cluster_url}/test_chef_cookbook/document/#{num} -d '{ "title": "Test #{num}", "time": "#{Time.now.utc}", "enabled": true }'|)
       end
+      system("curl --silent --show-error -X POST #{cluster_url}/test_chef_cookbook/_refresh")
 
-      Net::HTTP.post_form URI.parse("#{cluster_url}/test_chef_cookbook/_refresh"), {}
       resp = Net::HTTP.get_response URI.parse("#{cluster_url}/test_chef_cookbook/_search?q=Test")
       total_hits = JSON.parse(resp.read_body)['hits']['total']
 
