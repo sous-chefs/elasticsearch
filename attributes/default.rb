ccb510747206c5ca628e740ce3a55ac1dcdd88bd
Karel Minarik <karmi@karmi.cz>
2013-02-27 20:49:04 +0100

    [FIX] Fixed an issue where attributes set in `node.json` for Chef Solo were not picked up
    The settings specified in Vagrantfile were not properly picked up, since the
    `node.normal[:elasticsearch] = DeepMerge.merge ...` declaration overwrote them.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,9 @@ Chef::Log.debug "Loaded settings: #{settings.inspect}"
 # Initialize the node attributes with node attributes merged with data bag attributes
 #
 node.default[:elasticsearch] ||= {}
-node.normal[:elasticsearch]    = DeepMerge.merge(node.default[:elasticsearch].to_hash, settings.to_hash)
+node.normal[:elasticsearch]  ||= {}
+node.normal[:elasticsearch]    = DeepMerge.merge(node.default[:elasticsearch].to_hash, node.normal[:elasticsearch].to_hash)
+node.normal[:elasticsearch]    = DeepMerge.merge(node.normal[:elasticsearch].to_hash, settings.to_hash)
 
 # === VERSION AND LOCATION
 #
