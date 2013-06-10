beb8107b81c10456adcc6752a1b885bd9b9bc84a
Karel Minarik <karmi@karmi.cz>
2013-06-10 19:33:08 +0200

    Added a "elasticsearch::rpm" recipe to install from official RPM package

---
+++ attributes/rpm.rb
@@ -0,0 +1,2 @@
+default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.1.noarch.rpm"
+default.elasticsearch[:rpm_sha] = "a56eb8a88a15adad9ab8e59168c6cd51a1bd9ba3"
---
+++ recipes/rpm.rb
@@ -0,0 +1,11 @@
+filename = node.elasticsearch[:rpm_url].split('/').last
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
+  source   node.elasticsearch[:rpm_url]
+  checksum node.elasticsearch[:rpm_sha]
+  mode 00644
+end
+
+rpm_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+  action :install
+end
