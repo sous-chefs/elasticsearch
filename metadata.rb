be26b51f6c781536229e80ffd27e2e03970353a4
Karel Minarik <karmi@karmi.cz>
2013-06-10 19:58:27 +0200

    Release 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
+name             "elasticsearch"
+
 maintainer       "karmi"
 maintainer_email "karmi@karmi.cz"
 license          "Apache"
 description      "Installs and configures elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.2.7"
-name             "elasticsearch"
+version          "0.3.0"
 
 depends 'ark'
 
