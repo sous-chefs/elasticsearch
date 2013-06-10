869a44e99b82494ea07173b0334873b2941bc433
Karel Minarik <karmi@karmi.cz>
2013-06-10 16:37:06 +0200

    Fixed incorrect order of creating the plugins directory
    Based on changes from #80.

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,11 +1,11 @@
-node[:elasticsearch][:plugins].each do | name, config |
-  next if name == 'elasticsearch-cloud-aws' && !node.recipe?('aws')
-  install_plugin name, config
-end
-
 directory "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/" do
   owner node.elasticsearch[:user]
   group node.elasticsearch[:user]
   mode 0755
   recursive true
+end
+
+node[:elasticsearch][:plugins].each do | name, config |
+  next if name == 'elasticsearch-cloud-aws' && !node.recipe?('aws')
+  install_plugin name, config
 end
