2d98a820a76348a6e90c9d08794da30b1f0e3d7d
Karel Minarik <karmi@karmi.cz>
2013-06-10 19:22:15 +0200

    Added a "elasticsearch::deb" recipe to install from official Debian package

---
+++ attributes/deb.rb
@@ -0,0 +1,2 @@
+default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.1.deb"
+default.elasticsearch[:deb_sha] = "3ea1045b037912d9e2ebfbce5b6fc3aaffb77e5d"
---
+++ recipes/deb.rb
@@ -0,0 +1,11 @@
+filename = node.elasticsearch[:deb_url].split('/').last
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
+  source   node.elasticsearch[:deb_url]
+  checksum node.elasticsearch[:deb_sha]
+  mode 00644
+end
+
+dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+  action :install
+end
