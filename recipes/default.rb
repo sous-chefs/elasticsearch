320aaa06ed6d941503dbc50fe5d893d4e999779a
Gregoire Seux <g.seux@criteo.com>
2013-06-25 11:45:35 +0200

    Fix #101: idempotency of security limits

--- recipes/default.rb
+++ recipes/default.rb
@@ -94,20 +94,19 @@ bash "enable user limits" do
   not_if { ::File.read("/etc/pam.d/su").match(/^session    required   pam_limits\.so/) }
 end
 
-bash "increase limits for the elasticsearch user" do
-  user 'root'
+limits = node.elasticsearch[:limits].map do |limit,value|
+  "#{node.elasticsearch[:user]} - #{limit} #{value}"
+end
 
-  code <<-END.gsub(/^    /, '')
-    echo '#{node.elasticsearch.fetch(:user, "elasticsearch")}     -    nofile    #{node.elasticsearch[:limits][:nofile]}'  >> /etc/security/limits.conf
-    echo '#{node.elasticsearch.fetch(:user, "elasticsearch")}     -    memlock   #{node.elasticsearch[:limits][:memlock]}' >> /etc/security/limits.conf
-  END
-
-  not_if do
-    file = ::File.read("/etc/security/limits.conf")
-    file.include?("#{node.elasticsearch.fetch(:user, "elasticsearch")}     -    nofile    #{node.elasticsearch[:limits][:nofile]}") \
-    &&           \
-    file.include?("#{node.elasticsearch.fetch(:user, "elasticsearch")}     -    memlock   #{node.elasticsearch[:limits][:memlock]}")
-  end
+template "/etc/security/limits.d/elasticsearch.conf" do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  source 'security_limits.conf.erb'
+  variables({
+    :limits => limits
+  })
+  notifies :restart, 'service[elasticsearch]'
 end
 
 # Create file with ES environment variables
