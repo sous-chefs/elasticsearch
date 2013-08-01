42349ee7112de3295eb9cee1ee3edb0ca0566e0c
Karel Minarik <karmi@karmi.cz>
2013-08-01 11:01:07 +0200

    Fixed incorrect version notation for "cloud-aws" in `recipes/aws`
    Fixes #110

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -1,3 +1,3 @@
 node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['cloud-aws'])
 
-install_plugin "elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch-cloud-aws']['version']}"
+install_plugin "elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}"
