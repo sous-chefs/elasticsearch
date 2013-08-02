63d0b060e20356305b4cb70a2ddb6644ba3ddfc2
Riad Mohammed <rmohammed@Riads-MacBook-Pro.local>
2013-08-02 15:56:35 -0700

    Updated tests to use node attributes and for proxy_test not to run if the proxy recipe is not included

--- tests/proxy_test.rb
+++ tests/proxy_test.rb
@@ -10,27 +10,33 @@ describe_recipe 'elasticsearch::proxy' do
   proxy_url   = "http://USERNAME:PASSWORD@localhost:8080"
 
   it "runs as a daemon" do
-    service("nginx").must_be_running
+    if node.recipes.include?("elasticsearch::proxy")
+      service("nginx").must_be_running
+    end  
   end
 
   it "has a username in passwords file" do
-    file("/usr/local/etc/elasticsearch/passwords").must_exist.must_include("USERNAME")
+    if node.recipes.include?("elasticsearch::proxy")
+      file("#{node.elasticsearch[:nginx][:passwords_file]}").must_exist.must_include("USERNAME")
+    end  
   end
 
   it "proxies request to elasticsearch" do
-    timeout = 120
-    until system("curl --silent --show-error '#{health_url}?wait_for_status=yellow&timeout=1m' > /dev/null") or timeout == 0
-      sleep 1
-      timeout -= 1
-    end
+    if node.recipes.include?("elasticsearch::proxy")
+      timeout = 120
+      until system("curl --silent --show-error '#{health_url}?wait_for_status=yellow&timeout=1m' > /dev/null") or timeout == 0
+        sleep 1
+        timeout -= 1
+      end
 
-    Net::HTTP.start('localhost', 8080) do |http|
-      req = Net::HTTP::Get.new('/')
-      req.basic_auth 'USERNAME', 'PASSWORD'
-      response = http.request(req)
-      status = JSON.parse(response.body)['ok']
-      assert_equal status, true
-    end
+      Net::HTTP.start('localhost', 8080) do |http|
+        req = Net::HTTP::Get.new('/')
+        req.basic_auth 'USERNAME', 'PASSWORD'
+        response = http.request(req)
+        status = JSON.parse(response.body)['ok']
+        assert_equal status, true
+      end
+    end  
   end
 
 end
