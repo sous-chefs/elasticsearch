42df8eb900fcba55f4d655681333511795cef211
Andrew Gross <andrew.w.gross@gmail.com>
2013-09-16 12:23:53 -0400

    Fixing checksum
    Command `shasum -b -a 256 elasticsearch-0.90.4.deb`

--- attributes/deb.rb
+++ attributes/deb.rb
@@ -1,2 +1,2 @@
 default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.4.deb"
-default.elasticsearch[:deb_sha] = "f7615509343ee08dcc874e38267530f14da36fb6"
+default.elasticsearch[:deb_sha] = "5b11deb310aae745605e933a615e288a759d56277db88fe90cfcd1816c7f3a60"
