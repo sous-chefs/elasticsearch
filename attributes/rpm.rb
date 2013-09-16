5971680c38caa3cf5254de47f217a6a5948a4542
Andrew Gross <andrew.w.gross@gmail.com>
2013-09-16 12:15:45 -0400

    Update to 0.90.5

--- attributes/rpm.rb
+++ attributes/rpm.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.noarch.rpm"
-default.elasticsearch[:rpm_sha] = "a56eb8a88a15adad9ab8e59168c6cd51a1bd9ba3"
+default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.noarch.rpm"
+default.elasticsearch[:rpm_sha] = "a0d8b4cba30838f2ba09d960baf12e7c1aba6fd373c220e5f50416fc1c35e71d"
