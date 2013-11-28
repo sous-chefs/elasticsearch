9cc17cb37f2a9710e6887240c6147570da13afb0
Jean-Bernard DAMIANO <jbdamiano@gmail.com>
2013-11-28 14:23:11 +0100

    add option to be able to install deb package by apt

--- attributes/deb.rb
+++ attributes/deb.rb
@@ -1,2 +1,4 @@
 default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb"
 default.elasticsearch[:deb_sha] = "d25cdc971b1ef2393a620731137bae16e0d1107d49294b11c14634da0f1bb330"
+default.elasticsearch[:deb_type] = "source"
+
--- recipes/deb.rb
+++ recipes/deb.rb
@@ -1,30 +1,28 @@
 include_recipe "elasticsearch::_default"
 
 filename = node.elasticsearch[:deb_url].split('/').last
-   
-remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
-  source   node.elasticsearch[:deb_url]
-  checksum node.elasticsearch[:deb_sha]
-  mode 00644
-end
 
 service "elasticsearch" do
   supports :status => true, :restart => true, :stop => true
   action [ :nothing ]
 end
 
-dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
-  action :install
-  notifies  :stop, "service[elasticsearch]" , :immediately
+if node.elasticsearch[:deb_type] == "source"   
+  remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
+    source   node.elasticsearch[:deb_url]
+    checksum node.elasticsearch[:deb_sha]
+    mode 00644
+  end
+
+  dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+    action :install
+    notifies  :stop, "service[elasticsearch]" , :immediately
+  end
+else
+  package "elasticsearch" do
+    action :install
+    notifies  :stop, "service[elasticsearch]" , :immediately	
+  end
 end
 
 
-#use bash to stop service started automatically during the package installation
-# a notify to service['elasticsearch'] do it too late
-#bash "stop-service" do
-#  user "root"
-#  cwd "/tmp"
-#  code <<-EOH
-#  /etc/init.d/elasticsearch stop
-#  EOH
-#end
