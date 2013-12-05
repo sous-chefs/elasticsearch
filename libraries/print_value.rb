1ad0f5c347417e4eeb190132726e0e1c26fc43a4
Karel Minarik <karmi@karmi.cz>
2013-12-05 10:39:28 +0100

    Improved documentation for the `print_value` helper method
    Closes #169

--- libraries/print_value.rb
+++ libraries/print_value.rb
@@ -7,19 +7,19 @@ module Extensions
     #
     # 1. Using the key as a node attribute:
     #
-    #    <%= print_value 'bar' -%> is evaluated as: `node[:bar]`
+    #    <%= print_value 'bar' -%> is evaluated as: `node.elasticsearch[:bar]`
     #
     #    You may use a dot-separated key for nested attributes:
     #
     #    <%= print_value 'foo.bar' -%> is evaluated in multiple ways in this order:
     #
-    #    a) as `node['foo.bar']`,
-    #    b) as `node['foo_bar']`,
-    #    c) as `node.foo.bar` (ie. `node[:foo][:bar]`)
+    #    a) as `node.elasticsearch['foo.bar']`,
+    #    b) as `node.elasticsearch['foo_bar']`,
+    #    c) as `node.elasticsearch.foo.bar` (ie. `node.elasticsearch[:foo][:bar]`)
     #
     # 2. You may also provide an explicit value for the method, which is then used:
     #
-    #    <%= print_value 'bar', node[:foo] -%>
+    #    <%= print_value 'bar', node.elasticsearch[:foo] -%>
     #
     # You may pass a specific separator to the method:
     #
