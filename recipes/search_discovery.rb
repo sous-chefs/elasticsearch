0eeac9a6093ddd88c3d4b7eff050f3286324771c
Jean-Bernard DAMIANO <jbdamiano@gmail.com>
2013-12-12 07:56:16 +0100

    Update search_discovery.rb

--- recipes/search_discovery.rb
+++ recipes/search_discovery.rb
@@ -25,8 +25,8 @@ nodes = search_for_nodes(node['elasticsearch']['discovery']['search_query'],
 p node['elasticsearch']['discovery']['node_attribute']
 Chef::Log.debug("Found elasticsearch nodes at #{nodes.join(', ').inspect}")
 
-#first put the default value ipaddress
-nodes << "#{node.ipaddress}"
+#add local ip address in case of it is not found. First time start
+nodes << "#{node.ipaddress}" unless nodes.include?("#{node.ipaddress}")
 
 node.set['elasticsearch']['discovery']['zen']['ping']['unicast']['hosts'] = nodes.join(',')
 
