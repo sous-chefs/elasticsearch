80ce7cfe27ff851589acea9c58a4328705e45eac
Karel Minarik <karmi@karmi.cz>
2014-02-03 15:39:17 +0100

    Updated the default Elasticsearch version to 0.90.10
    See: http://www.elasticsearch.org/downloads/0-90-10/

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,7 +13,7 @@ node.normal[:elasticsearch]    = DeepMerge.merge(node.normal[:elasticsearch].to_
 
 # === VERSION AND LOCATION
 #
-default.elasticsearch[:version]       = "0.90.5"
+default.elasticsearch[:version]       = "0.90.10"
 default.elasticsearch[:host]          = "http://download.elasticsearch.org"
 default.elasticsearch[:repository]    = "elasticsearch/elasticsearch"
 default.elasticsearch[:filename]      = "elasticsearch-#{node.elasticsearch[:version]}.tar.gz"
