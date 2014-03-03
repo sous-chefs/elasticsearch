02caca6d8698ca3b3ef6ba79686e41ce1fec097f
Karel Minarik <karmi@karmi.cz>
2014-03-03 10:57:20 +0100

    Updated Elasticsearch to 0.90.12 in the "elasticsearch::rpm" recipe

--- attributes/rpm.rb
+++ attributes/rpm.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.noarch.rpm"
-default.elasticsearch[:rpm_sha] = "a0d8b4cba30838f2ba09d960baf12e7c1aba6fd373c220e5f50416fc1c35e71d"
+default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.12.noarch.rpm"
+default.elasticsearch[:rpm_sha] = "ab7ea2e00d8f1b73642e3ea44d9647b11e6b0b96"
