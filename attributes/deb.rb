b9579f8c1e3a902c88430ff7db00b9f5a708f717
Karel Minarik <karmi@karmi.cz>
2014-03-03 10:56:48 +0100

    Updated Elasticsearch to 0.90.12 in the "elasticsearch::deb" recipe

--- attributes/deb.rb
+++ attributes/deb.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.10.deb"
-default.elasticsearch[:deb_sha] = "599a8ff133f0800bb58032f1bf4ec0b1746c307befb57c65ee9d32af9f407c04"
+default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.12.deb"
+default.elasticsearch[:deb_sha] = "13783b95b35e347a3471f22234c544ae50655b78"
