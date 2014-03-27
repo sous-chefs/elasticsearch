15c51a61abd659943d4879822f773bd8e5e672cf
Karel Minarik <karmi@karmi.cz>
2014-03-27 16:37:38 +0100

    Release 0.3.8

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "karmi@karmi.cz"
 license          "Apache"
 description      "Installs and configures elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.3.7"
+version          "0.3.8"
 
 depends 'ark', '>= 0.2.4'
 
