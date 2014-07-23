f47bc179b9116ca7f58f0aa8d76efc2f4e70ae23
Stuart Warren <stuart.warren@ocado.com>
2014-07-23 15:06:07 +0100

    comment example attributes

--- attributes/curator.rb
+++ attributes/curator.rb
@@ -3,10 +3,11 @@
 #
 default.elasticsearch[:curator][:version] = "1.1.3"
 
-default.elasticsearch[:curator][:cron][:basic][:action] = :create
-default.elasticsearch[:curator][:cron][:basic][:minute] = '0'
-default.elasticsearch[:curator][:cron][:basic][:hour] = '0'
-default.elasticsearch[:curator][:cron][:basic][:day] = '*'
-default.elasticsearch[:curator][:cron][:basic][:weekday] = '*'
-default.elasticsearch[:curator][:cron][:basic][:month] = '*'
-default.elasticsearch[:curator][:cron][:basic][:command] = 'curator bloom --older-than 1 ; curator close --older-than 15 ; curator optimize --older-than 1 --max_num_segments 1'
+# "basic" is just a name so you can create more than one entry
+# default.elasticsearch[:curator][:cron][:basic][:action] = :create
+# default.elasticsearch[:curator][:cron][:basic][:minute] = '0'
+# default.elasticsearch[:curator][:cron][:basic][:hour] = '0'
+# default.elasticsearch[:curator][:cron][:basic][:day] = '*'
+# default.elasticsearch[:curator][:cron][:basic][:weekday] = '*'
+# default.elasticsearch[:curator][:cron][:basic][:month] = '*'
+# default.elasticsearch[:curator][:cron][:basic][:command] = 'curator bloom --older-than 1 ; curator close --older-than 15 ; curator optimize --older-than 1 --max_num_segments 1'
