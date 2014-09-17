4e55d655dae3850974ef857bda4d4b4b7b1a328f
jakekdodd <jakekdodd@users.noreply.github.com>
2014-09-17 12:18:46 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -78,8 +78,9 @@ end
 #
 ark_prefix_root = node.elasticsearch[:dir] || node.ark[:prefix_root]
 ark_prefix_home = node.elasticsearch[:dir] || node.ark[:prefix_home]
+filename = node.elasticsearch[:filename] || "elasticsearch-#{node.elasticsearch[:version]}.tar.gz"
 download_url = node.elasticsearch[:download_url] || [node.elasticsearch[:host],
-                node.elasticsearch[:repository], node.elasticsearch[:filename]].join('/')
+                node.elasticsearch[:repository], filename].join('/')
 
 ark "elasticsearch" do
   url   download_url
