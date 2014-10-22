84f7b2db9b0ba99467a2f29d9f216e73afe5b454
Karel Minarik <karmi@karmi.cz>
2014-10-22 17:05:43 +0200

    Changed the maintainer information in metadata.rb files

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 # Encoding: utf-8
 name             'elasticsearch'
-maintainer       'karmi'
-maintainer_email 'karmi@karmi.cz'
+maintainer       'Karel Minarik'
+maintainer_email 'karel.minarik@elasticsearch.org'
 license          'Apache 2.0'
-description      'Installs/Configures elasticsearch'
+description      'Installs and configures Elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
