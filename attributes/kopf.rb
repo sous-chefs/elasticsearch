67fdf00c90e75d339571009bf105b46e53fdfc75
Javier Arrieta <jarrieta@gilt.com>
2014-11-14 11:12:22 +0000

    Fix kopf variable name because of copy/paste

--- attributes/kopf.rb
+++ attributes/kopf.rb
@@ -1,6 +1,6 @@
 include_attribute 'elasticsearch::default'
 include_attribute 'elasticsearch::plugins'
 
-aws = Chef::DataBagItem.load('elasticsearch', 'kopf')[node.chef_environment] rescue {}
+kopf = Chef::DataBagItem.load('elasticsearch', 'kopf')[node.chef_environment] rescue {}
 
 default.elasticsearch['pluginslmenezes/elasticsearch-kopf']['version'] = '1.3.8'
