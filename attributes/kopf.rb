95ab6976e50ca838c8f52f53a9e4cd0d04de5935
Javier Arrieta <jarrieta@gilt.com>
2014-12-17 16:12:23 +0000

    Fix kopf plugin attribute name

--- attributes/kopf.rb
+++ attributes/kopf.rb
@@ -3,4 +3,4 @@ include_attribute 'elasticsearch::plugins'
 
 kopf = Chef::DataBagItem.load('elasticsearch', 'kopf')[node.chef_environment] rescue {}
 
-default.elasticsearch['pluginslmenezes/elasticsearch-kopf']['version'] = '1.3.8'
+default.elasticsearch['plugins']['kopf']['version'] = '1.4.0'
--- recipes/kopf.rb
+++ recipes/kopf.rb
@@ -1,3 +1,3 @@
 node.default[:elasticsearch][:plugin][:mandatory] = Array(node[:elasticsearch][:plugin][:mandatory] | ['kopf'])
 
-install_plugin "lmenezes/elasticsearch-kopf/#{node.elasticsearch['plugins']['lmenezes/elasticsearch-kopf']['version']}"
+install_plugin "lmenezes/elasticsearch-kopf/#{node.elasticsearch['plugins']['kopf']['version']}"
