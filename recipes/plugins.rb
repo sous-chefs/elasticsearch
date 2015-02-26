95669bfd772d0b711ba0ee90fdb7f64acb8fe1d6
Richard Owen <richard.owen@huddle.com>
2015-02-26 14:23:37 +0000

    Don't install the cloud-gce plugin by default
    Closes #304

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -7,5 +7,6 @@ end
 
 node[:elasticsearch][:plugins].each do | name, config |
   next if name == 'elasticsearch/elasticsearch-cloud-aws' && !node.recipe?('aws')
+  next if name == 'elasticsearch/elasticsearch-cloud-gce' && !node.recipe?('gce')
   install_plugin name, config
 end
