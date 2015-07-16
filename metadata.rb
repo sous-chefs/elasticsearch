eea1bd17f07cf83495d2e61cef39723a4e274c64
Martin Smith <martin@mbs3.org>
2015-07-16 08:17:12 -0500

    Release final 0.3.x - 0.3.14

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "karmi@karmi.cz"
 license          "Apache"
 description      "Installs and configures elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.3.13"
+version          "0.3.14"
 
 depends 'ark', '>= 0.2.4'
 
