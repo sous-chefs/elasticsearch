d43dfc99136923e0a46231fa6e95efc8f4836983
Thomas Dziedzic <thomas@pagerduty.com>
2015-09-16 13:40:02 -0700

    add missing matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -13,4 +13,22 @@ if defined?(ChefSpec)
   def remove_elasticsearch(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_install, :remove, resource_name)
   end
+  def manage_elasticsearch_configure(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_configure, :manage, resource_name)
+  end
+  def remove_elasticsearch_configure(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_configure, :remove, resource_name)
+  end
+  def configure_elasticsearch_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_service, :configure, resource_name)
+  end
+  def remove_elasticsearch_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_service, :remove, resource_name)
+  end
+  def install_elasticsearch_plugin(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_plugin, :install, resource_name)
+  end
+  def remove_elasticsearch_plugin(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:elasticsearch_plugin, :remove, resource_name)
+  end
 end
