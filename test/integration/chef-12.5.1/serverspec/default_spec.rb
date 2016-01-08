a915a997768e0b6aea033f310857af35e608905b
Martin Smith <martin@mbs3.org>
2016-01-08 11:56:54 -0600

    Test with Chef 12.6.0
    - Add Chef 12.6.0 for tests
    - Move down Chef 12.5.1 as one of the 'older chef' suites

---
+++ test/integration/chef-12.5.1/serverspec/default_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe 'standard elasticsearch install and configure' do
+  it_behaves_like 'elasticsearch user'
+  it_behaves_like 'elasticsearch install'
+  it_behaves_like 'elasticsearch configure'
+  it_behaves_like 'elasticsearch plugin', 'head'
+  it_behaves_like 'elasticsearch service'
+
+  it_behaves_like 'chef version', '12.5.1'
+end
