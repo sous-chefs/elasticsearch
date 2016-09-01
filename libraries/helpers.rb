642533d0c43a7df360b6dcaf57a41c5c51af4f76
Martin Smith <martin@mbs3.org>
2016-09-01 15:28:36 +0200

    Update metadata and linting

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -6,32 +6,16 @@ module ElasticsearchCookbook
       instance_name = resource.instance_name
 
       # if we are truly given a specific name to find
-      name_match = begin
-                     find_exact_resource(run_context, resource_type, resource_name)
-                   rescue
-                     nil
-                   end
+      name_match = find_exact_resource(run_context, resource_type, resource_name) rescue nil
       return name_match if name_match
 
       # first try by instance name attribute
-      name_instance = begin
-                        find_instance_name_resource(run_context, resource_type, instance_name)
-                      rescue
-                        nil
-                      end
+      name_instance = find_instance_name_resource(run_context, resource_type, instance_name) rescue nil
       return name_instance if name_instance
 
       # otherwise try the defaults
-      name_default = begin
-                       find_exact_resource(run_context, resource_type, 'default')
-                     rescue
-                       nil
-                     end
-      name_elasticsearch = begin
-                             find_exact_resource(run_context, resource_type, 'elasticsearch')
-                           rescue
-                             nil
-                           end
+      name_default = find_exact_resource(run_context, resource_type, 'default') rescue nil
+      name_elasticsearch = find_exact_resource(run_context, resource_type, 'elasticsearch') rescue nil
 
       # if we found exactly one default name that matched
       return name_default if name_default && !name_elasticsearch
--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,20 @@ license          'Apache 2.0'
 description      'Installs and configures Elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.3.2'
-issues_url       'https://github.com/elastic/cookbook-elasticsearch/issues'
-source_url       'https://github.com/elastic/cookbook-elasticsearch'
+
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'redhat'
+supports 'ubuntu'
 
 depends 'apt'
 depends 'yum'
 depends 'chef-sugar'
 depends 'ark'
+
+issues_url       'https://github.com/elastic/cookbook-elasticsearch/issues'
+source_url       'https://github.com/elastic/cookbook-elasticsearch'
+
+chef_version '>= 12.4' if respond_to?(:chef_version)
