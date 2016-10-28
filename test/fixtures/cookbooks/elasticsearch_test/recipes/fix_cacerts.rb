9d92755315addfa1de4c38317b09a01d2261b720
Martin Smith <martin@mbs3.org>
2016-10-28 11:31:27 +0800

    Add support for Ubuntu 16.04 / CentOS 7.2
    - Add support for Ubuntu 16.04 and testing. Also had to add a 'fixer' recipe for corrupted trust stores in some default images from the bento project.
    - Add support for CentOS 7.2 and testing.
    
    I was surprised to find the init scripts we already had worked flawlessly.
    
    Fixes #501.
    Fixes #502.

---
+++ test/fixtures/cookbooks/elasticsearch_test/recipes/fix_cacerts.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: elasticsearch_test
+# Recipe:: fix_cacerts
+#
+
+# apparently some distros of Ubuntu don't have good SSL trust stores by default.
+execute 'update-ca-certificates -f' do
+  only_if 'which update-ca-certificates'
+end
