dedf033488715d22909ad940d272a3e6670a8129
Léo Cavaillé <leo@datadoghq.com>
2016-11-16 12:36:08 +0100

    ES 2.4.1 checksums + use for install/tests default

--- test/integration/helpers/serverspec/install_examples.rb
+++ test/integration/helpers/serverspec/install_examples.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 shared_examples_for 'elasticsearch install' do |args = {}|
   dir = args[:dir] || (package? ? '/usr/share/elasticsearch' : '/usr/local')
-  version = args[:version] || '2.4.0'
+  version = args[:version] || '2.4.1'
 
   expected_user = args[:user] || 'elasticsearch'
   expected_group = args[:group] || expected_user || 'elasticsearch'
