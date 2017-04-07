2a229ae069dd721b753bc052a8a4472e1469bca6
Thomas Vendetta <tvendetta@weblinc.com>
2017-04-07 14:06:16 -0400

    Default chef_proxy to false

--- libraries/resource_plugin.rb
+++ libraries/resource_plugin.rb
@@ -11,7 +11,7 @@ class ElasticsearchCookbook::PluginResource < Chef::Resource::LWRPBase
   # if the name or url are different from the resource name
   attribute(:plugin_name, kind_of: String, name_attribute: true)
   attribute(:url, kind_of: String, default: nil)
-  attribute(:chef_proxy, kind_of: [TrueClass, FalseClass], default: true)
+  attribute(:chef_proxy, kind_of: [TrueClass, FalseClass], default: false)
   attribute(:options, kind_of: String, default: '')
 
   # this is what helps the various resources find each other
