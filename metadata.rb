e574e5eb1abd349882ed495ea357277d657188ae
brianvans <brianvans@gmail.com>
2019-05-18 18:54:24 -0700

    Use standardized license in metadata
    Fixes `FC069: Ensure standardized license defined in metadata`

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@
 name             'elasticsearch'
 maintainer       'Karel Minarik'
 maintainer_email 'karel.minarik@elasticsearch.org'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures Elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.5'
