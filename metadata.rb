b900ef6c119defd083a19a0eda77539952ad3886
Martin Smith <martin@mbs3.org>
2019-12-30 22:37:07 -0500

    Cleanup some Cookstyle complaints

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer       'Karel Minarik'
 maintainer_email 'karel.minarik@elasticsearch.org'
 license          'Apache-2.0'
 description      'Installs and configures Elasticsearch'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.2.0'
 
 supports 'amazon'
