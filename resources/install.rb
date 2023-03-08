1d67f773c4e9c33966cb922f26d0c31fc2294bd8
Dan Webb <dan.webb@damacus.io>
2023-03-08 22:05:50 +0000

    [install] Stop using updated_by_last_action

--- resources/install.rb
+++ resources/install.rb
@@ -68,9 +68,12 @@ action_class do
 
     if new_resource.enable_repository_actions
       if platform_family?('debian')
-        apt_r = apt_repo_resource
-        apt_r.run_action(:add)
-        new_resource.updated_by_last_action(true) if apt_r.updated_by_last_action?
+        apt_repository "elastic-#{new_resource.version.to_i}.x" do
+          key "elasticsearch.asc"
+          cookbook 'elasticsearch'
+          components ['main']
+          distribution 'stable'
+        end
       else
         yr_r = yum_repo_resource
         yr_r.run_action(:create)
@@ -78,22 +81,19 @@ action_class do
       end
     end
 
-    pkg_r = package 'elasticsearch' do
+    package 'elasticsearch' do
       options new_resource.package_options
       version new_resource.version
-      action :nothing
+      action :install
     end
-
-    pkg_r.run_action(:install)
-    new_resource.updated_by_last_action(true) if pkg_r.updated_by_last_action?
   end
 
   def remove_repo_wrapper_action
     if new_resource.enable_repository_actions
       if platform_family?('debian')
-        apt_r = apt_repo_resource
-        apt_r.run_action(:remove)
-        new_resource.updated_by_last_action(true) if apt_r.updated_by_last_action?
+        apt_repository "elastic-#{new_resource.version.to_i}.x" do
+          action :remove
+        end
       else
         yr_r = yum_repo_resource
         yr_r.run_action(:delete)
@@ -101,25 +101,23 @@ action_class do
       end
     end
 
-    pkg_r = package 'elasticsearch' do
+    package 'elasticsearch' do
       options new_resource.package_options
       version new_resource.version
-      action :nothing
+      action :remove
     end
-    pkg_r.run_action(:remove)
-    new_resource.updated_by_last_action(true) if pkg_r.updated_by_last_action?
   end
 
   def install_package_wrapper_action
     es_user = find_es_resource(Chef.run_context, :elasticsearch_user, new_resource)
+
     unless es_user && es_user.username == 'elasticsearch' && es_user.groupname == 'elasticsearch'
       raise 'Custom usernames/group names is not supported in Elasticsearch 6+ package installation'
     end
 
     found_download_url = determine_download_url(new_resource, node)
-    unless found_download_url
-      raise 'Could not determine download url for package on this platform'
-    end
+
+    raise 'Could not determine download url for package on this platform' unless found_download_url
 
     filename = found_download_url.split('/').last
     checksum = determine_download_checksum(new_resource, node)
@@ -129,32 +127,24 @@ action_class do
       Chef::Log.warn("No checksum was provided for #{found_download_url}, this may download a new package on every chef run!")
     end
 
-    remote_file_r = remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
+    remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
       source found_download_url
       checksum checksum
       mode '0644'
-      action :nothing
+      action :create
     end
 
-    remote_file_r.run_action(:create)
-
-    new_resource.updated_by_last_action(true) if remote_file_r.updated_by_last_action?
-
-    pkg_r = if platform_family?('debian')
-              dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
-                options package_options
-                action :nothing
-              end
-            else
-              package "#{Chef::Config[:file_cache_path]}/#{filename}" do
-                options package_options
-                action :nothing
-              end
-            end
-
-    pkg_r.run_action(:install)
-
-    new_resource.updated_by_last_action(true) if pkg_r.updated_by_last_action?
+    if platform_family?('debian')
+      dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+        options package_options
+        action :install
+      end
+    else
+      package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+        options package_options
+        action :install
+      end
+    end
   end
 
   def remove_package_wrapper_action
@@ -162,31 +152,24 @@ action_class do
 
     filename = package_url.split('/').last
 
-    pkg_r = if platform_family?('debian')
-              dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
-                action :nothing
-              end
-            else
-              package "#{Chef::Config[:file_cache_path]}/#{filename}" do
-                action :nothing
-              end
-            end
-
-    pkg_r.run_action(:remove)
-
-    new_resource.updated_by_last_action(true) if pkg_r.updated_by_last_action?
+    if platform_family?('debian')
+      dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+        action :temove
+      end
+    else
+      package "#{Chef::Config[:file_cache_path]}/#{filename}" do
+        action :temove
+      end
+    end
   end
 
   def install_tarball_wrapper_action
-    include_recipe 'ark'
-
     es_user = find_es_resource(Chef.run_context, :elasticsearch_user, new_resource)
     found_download_url = determine_download_url(new_resource, node)
-    unless found_download_url
-      raise 'Could not determine download url for tarball on this platform'
-    end
 
-    ark_r = ark 'elasticsearch' do
+    raise 'Could not determine download url for tarball on this platform'  unless found_download_url
+
+    ark 'elasticsearch' do
       url   found_download_url
       owner es_user.username
       group es_user.groupname
@@ -203,43 +186,35 @@ action_class do
 
         ::File.directory?(link) && ::File.symlink?(link) && ::File.readlink(link) == target && ::File.exist?(binary)
       end
-      action :nothing
+      action :install
     end
-    ark_r.run_action(:install)
-    new_resource.updated_by_last_action(true) if ark_r.updated_by_last_action?
 
-    # destroy the sample config directory for tarball installs, or it will
+    # Delete the sample config directory for tarball installs, or it will
     # take precedence beyond the default stuff in /etc/elasticsearch and within
     # /etc/sysconfig or /etc/default
-    sample_r = directory "#{new_resource.dir}/elasticsearch/config" do
-      action :nothing
+    directory "#{new_resource.dir}/elasticsearch/config" do
+      action :delete
       recursive true
     end
-    sample_r.run_action(:delete)
-    new_resource.updated_by_last_action(true) if sample_r.updated_by_last_action?
   end
 
   def remove_tarball_wrapper_action
     # remove the symlink to this version
-    link_r = link "#{new_resource.dir}/elasticsearch" do
+    link "#{new_resource.dir}/elasticsearch" do
       only_if do
         link   = "#{new_resource.dir}/elasticsearch"
         target = "#{new_resource.dir}/elasticsearch-#{new_resource.version}"
 
         ::File.directory?(link) && ::File.symlink?(link) && ::File.readlink(link) == target
       end
-      action :nothing
+      action :delete
     end
-    link_r.run_action(:delete)
-    new_resource.updated_by_last_action(true) if link_r.updated_by_last_action?
 
     # remove the specific version
-    d_r = directory "#{new_resource.dir}/elasticsearch-#{new_resource.version}" do
+    directory "#{new_resource.dir}/elasticsearch-#{new_resource.version}" do
       recursive true
-      action :nothing
+      action :delete
     end
-    d_r.run_action(:delete)
-    new_resource.updated_by_last_action(true) if d_r.updated_by_last_action?
   end
 
   def yum_repo_resource
