98d29f3cf19c5e20df00ef354fd27a073c0589ee
Nils Ratusznik <nils.github@anotherhomepage.org>
2025-10-10 15:11:42 +0200

    Add systemd unit option that prevents service from failing to start on Debian and Elasticsearch 8.x.

--- resources/service.rb
+++ resources/service.rb
@@ -40,6 +40,7 @@ action :configure do
       },
       Service: {
         Type: 'notify',
+        NotifyAccess: 'all',
         RuntimeDirectory: 'elasticsearch',
         PrivateTmp: 'true',
         Environment: [
